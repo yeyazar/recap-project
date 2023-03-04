c45ea2100ec16779986eade0ce11520502251016
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-04 12:31:49 +0100

    pushlamiyorrr

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -4,56 +4,56 @@ const ProductForm = ({ formData,handleChange,handleSubmit }) => {
   return (
     <article id="add-product" className="mb-4 mt-4">
       <h1 className="text-center">Product</h1>
-      <form class="p-2" onSubmit={handleSubmit}>
-        <div class="mb-3">
-          <label for="add-name" class="form-label">
+      <form className="p-2" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="add-name" className="form-label">
             Product Name
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="name"
             value={formData.name}
             onChange={handleChange}
             required
           />
         </div>
-        <div class="mb-3">
-          <label for="add-price" class="form-label">
+        <div className="mb-3">
+          <label htmlFor="add-price" className="form-label">
             Product Price
           </label>
           <input
             type="number"
-            class="form-control"
+            className="form-control"
             id="price"
             value={formData.price}
             onChange={handleChange}
             required
           />
         </div>
-        <div class="mb-3">
-          <label for="add-quantity" class="form-label">
+        <div className="mb-3">
+          <label htmlFor="add-quantity" className="form-label">
             Product Quantity
           </label>
           <input
             type="number"
-            class="form-control"
+            className="form-control"
             id="amount"
             value={formData.amount}
             onChange={handleChange}
             required
           />
         </div>
-        <label for="add-image" class="form-label">
+        <label htmlFor="add-image" className="form-label">
           Product Image
         </label>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon3">
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon3">
             https://example.com/
           </span>
           <input
             type="url"
-            class="form-control"
+            className="form-control"
             id="image"
             value={formData.image}
             onChange={handleChange}
@@ -61,9 +61,9 @@ const ProductForm = ({ formData,handleChange,handleSubmit }) => {
             required
           />
         </div>
-        <div class="text-center">
-          <button type="submit" class="add-to-cart btn btn-success btn-sm">
-            <i class="fa-solid fa-cart-plus me-2"></i>Save To Product
+        <div className="text-center">
+          <button type="submit" className="add-to-cart btn btn-success btn-sm">
+            <i className="fa-solid fa-cart-plus me-2"></i>Save To Product
           </button>
         </div>
       </form>
--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -16,6 +16,7 @@ const NewProduct = () => {
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log({[e.target.id]: e.target.value});
   };
 
   const handleSubmit = async (e) => {
